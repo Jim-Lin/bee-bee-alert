eaf6803c5c9fbb381ae39416672409f9000d4966
Jim-Lin <acgsong.tw@yahoo.com.tw>
2018-05-17 15:41:22 +0800

    Add getClient function

--- backend/db/redis.go
+++ backend/db/redis.go
@@ -10,36 +10,24 @@ import (
 	"github.com/Jim-Lin/bee-bee-alert/backend/util"
 )
 
-var redisClient *redis.Client
-
-func init() {
-	redisClient = redis.NewClient(&redis.Options{
-		Addr:         util.GetConfig().RedisUrl,
-		DialTimeout:  10 * time.Second,
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 30 * time.Second,
-		PoolSize:     10,
-		PoolTimeout:  30 * time.Second,
-	})
-	redisClient.FlushDB()
-}
+var client *redis.Client
 
 func IncrCounter(prod model.Prod) {
 	key := prod.Url
-	result, err := redisClient.Incr(key).Result()
+	result, err := getClient().Incr(key).Result()
 	util.CheckError(err)
 
 	// https://stackoverflow.com/a/49251938/4436392
 	if result == 30 {
-		pipe := redisClient.Pipeline()
-		pipe.Expire(key, 10 * time.Second)
+		pipe := getClient().Pipeline()
+		pipe.Expire(key, 30 * time.Second)
 		_, err := pipe.Exec()
 		util.CheckError(err)
 	}
 
-	if result > 1 {
+	if result > 10 {
 		log.Println(key)
-		_, err := redisClient.Del(key).Result()
+		_, err := getClient().Del(key).Result()
 		util.CheckError(err)
 
 		go (&util.MailTemplate{
@@ -49,4 +37,20 @@ func IncrCounter(prod model.Prod) {
 		GetMail().
 		Notify()
 	}
+}
+
+func getClient() *redis.Client {
+  if client == nil {
+		client = redis.NewClient(&redis.Options{
+			Addr:         util.GetConfig().RedisUrl,
+			DialTimeout:  10 * time.Second,
+			ReadTimeout:  30 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			PoolSize:     10,
+			PoolTimeout:  30 * time.Second,
+		})
+		client.FlushDB()
+  }
+
+  return client
 }
