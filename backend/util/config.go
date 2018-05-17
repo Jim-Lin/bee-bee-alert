348f84e17556d28d23a02979f9039b1f904bab33
Jim-Lin <acgsong.tw@yahoo.com.tw>
2018-05-17 15:41:54 +0800

    Modify to singleton

--- backend/util/config.go
+++ backend/util/config.go
@@ -18,24 +18,14 @@ type Config struct {
 var cfg Config
 
 // singleton pattern
-// func GetConfig() Config {
-//   if cfg == (Config{}) {
-//     abspath, _ := filepath.Abs("./")
-//     p := properties.MustLoadFile(abspath + "/backend.properties", properties.UTF8)
-//
-//     err := p.Decode(&cfg)
-//     CheckError(err)
-//   }
-//
-//   return cfg
-// }
-
 func GetConfig() Config {
-  abspath, _ := filepath.Abs("./")
-  p := properties.MustLoadFile(abspath + "/backend.properties", properties.UTF8)
+  if cfg == (Config{}) {
+    abspath, _ := filepath.Abs("./")
+    p := properties.MustLoadFile(abspath + "/backend.properties", properties.UTF8)
 
-  err := p.Decode(&cfg)
-  CheckError(err)
+    err := p.Decode(&cfg)
+    CheckError(err)
+  }
 
   return cfg
 }
